0e697df89f801566a185ae05ef6e9abd232fa8ac
Andrew Zimmerman <astevezimm@gmail.com>
2025-10-22 15:36:42 -0600

    initial structure for RefCardDraftPage.tsx

--- app/routes/$gameId/RefCardDraftPage.tsx
+++ app/routes/$gameId/RefCardDraftPage.tsx
@@ -1,5 +1,16 @@
 import {DraftPageContentProps} from '~/routes/$gameId/route'
 
 export default function RefCardDraftPage({playerSelected, selectedPlayer, state}: DraftPageContentProps) {
-  return null
+  return (
+    <>
+      <h2>Select main game faction to draft. Rest will be passed.</h2>
+      <h3>One choice for seating initiative (lowest is speaker), one for home system, and one for starting units.</h3>
+      <div className="ref-card-draft card main-section">
+        <h2>Options</h2>
+      </div>
+      <div className="ref-card-draft card main-section">
+        <h2>Your Drafted Cards</h2>
+      </div>
+    </>
+  )
 }
